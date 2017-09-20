150c448379ca06ff723bf0f9c5ed3d5433172139
Ramesh Kudiala <ramesh.k@alpharooms.com>
2017-09-20 14:33:41 +0530

    sdfsfsfssdfsfsfcvbcbvcbcvbbcbcergtergteg

---
+++ TestProject/src/com/demo/tests/Method1.java
@@ -0,0 +1,8 @@
+package com.demo.tests;
+
+public class Method1 {
+	public void test(){
+		System.out.println("Method one  test");
+	}
+
+}
---
+++ TestProject/src/com/demo/tests/Method2.java
@@ -0,0 +1,7 @@
+package com.demo.tests;
+
+public class Method2 {
+	public void test(){
+		System.out.println("Method two  test");
+	}
+}
