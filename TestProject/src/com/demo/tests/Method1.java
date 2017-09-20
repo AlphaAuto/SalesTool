41b606b18b4841c3892aa1bcc5822f9de904ac7d
AlphaAuto <testautomation73@gmail.com>
2017-09-20 14:42:16 +0530

    Updating method1
    No conflits

--- TestProject/src/com/demo/tests/Method1.java
+++ TestProject/src/com/demo/tests/Method1.java
@@ -3,6 +3,9 @@ package com.demo.tests;
 public class Method1 {
 	public void test(){
 		System.out.println("Method one  test");
+		System.out.println("Method one  test1");
+		System.out.println("Method one  test2");
+		System.out.println("Method one  test3");
 	}
 
 }
